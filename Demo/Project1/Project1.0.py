6ba280b4dc91c0bd7145eb3cdb3c3598b9c13938
DPurlix459 <dasha.ajufoh@binghamuni.edu.ng>
2024-11-01 22:02:59 +0100

    1st Commit.

---
+++ Demo/Project1/Project1.0.py
@@ -0,0 +1,18 @@
+age=16
+jamb_score=219
+
+
+
+if age >16 or jamb_score >219:
+    print(" Congrats!!You are qualified for this application into Bingham University.")
+else:
+    print("Oops!!Sorry but specified data does not qualify for your application into Bingham University.")
+
+
+num1=1650
+num2=12
+num3=5
+num4=12
+
+print(num1+num3)
+print(num2+num4)
